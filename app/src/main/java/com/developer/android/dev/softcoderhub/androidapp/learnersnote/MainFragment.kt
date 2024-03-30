8d52cec90270a363ed40350af4d7584e66ba4374
Devajit <patar1447@gmail.com>
2024-03-30 17:55:57 +0530

    Implement Interceptor to add a Header to the Request using OkHttpClient

--- app/src/main/java/com/developer/android/dev/softcoderhub/androidapp/learnersnote/MainFragment.kt
+++ app/src/main/java/com/developer/android/dev/softcoderhub/androidapp/learnersnote/MainFragment.kt
@@ -1,26 +1,33 @@
 package com.developer.android.dev.softcoderhub.androidapp.learnersnote
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.developer.android.dev.softcoderhub.androidapp.learnersnote.api.NotesAPI
+import com.developer.android.dev.softcoderhub.androidapp.learnersnote.utils.Constant.TAG
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+    @Inject
+    lateinit var notesAPI: NotesAPI
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = notesAPI.getNotes()
+            Log.d(TAG,response.body().toString())
+        }
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
--- app/src/main/java/com/developer/android/dev/softcoderhub/androidapp/learnersnote/di/NetworkModule.kt
+++ app/src/main/java/com/developer/android/dev/softcoderhub/androidapp/learnersnote/di/NetworkModule.kt
@@ -1,14 +1,20 @@
 package com.developer.android.dev.softcoderhub.androidapp.learnersnote.di
 
+import com.developer.android.dev.softcoderhub.androidapp.learnersnote.api.AuthInterceptor
+import com.developer.android.dev.softcoderhub.androidapp.learnersnote.api.NotesAPI
 import com.developer.android.dev.softcoderhub.androidapp.learnersnote.api.UserAPI
 import com.developer.android.dev.softcoderhub.androidapp.learnersnote.utils.Constant.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.Retrofit.Builder
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
+import javax.xml.transform.OutputKeys
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -16,16 +22,31 @@ class NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit():Retrofit{
+    fun provideRetrofitBuilder():Retrofit.Builder{
         return  Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(BASE_URL)
-            .build()
     }
 
     @Singleton
     @Provides
-    fun provideUserAPI(retrofit: Retrofit):UserAPI{
-        return  retrofit.create(UserAPI::class.java)
+    fun provideOkHttpClient(authInterceptor: AuthInterceptor):OkHttpClient{
+        return OkHttpClient.Builder()
+            .addInterceptor(authInterceptor).build()
+    }
+    @Singleton
+    @Provides
+    fun provideUserAPI(retrofitBuilder: Retrofit.Builder):UserAPI{
+        return  retrofitBuilder
+            .build().create(UserAPI::class.java)
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideNoteAPI(retrofitBuilder: Retrofit.Builder,okHttpClient: OkHttpClient):NotesAPI{
+        return retrofitBuilder
+            .client(okHttpClient)
+            .build().create(NotesAPI::class.java)
     }
 }
