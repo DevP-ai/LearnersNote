e37320b807aa27f201f8f46016bb930d77568153
Devajit <patar1447@gmail.com>
2024-03-30 16:17:52 +0530

    Retrofit EndPoints for Notes are implemented

---
+++ app/src/main/java/com/developer/android/dev/softcoderhub/androidapp/learnersnote/api/NotesAPI.kt
@@ -0,0 +1,27 @@
+package com.developer.android.dev.softcoderhub.androidapp.learnersnote.api
+
+import com.developer.android.dev.softcoderhub.androidapp.learnersnote.models.NoteRequest
+import com.developer.android.dev.softcoderhub.androidapp.learnersnote.models.NoteResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+
+interface NotesAPI {
+
+    @GET("/note")
+    suspend fun getNotes():Response<List<NoteResponse>>
+
+    @POST("/note")
+    suspend fun createNote(@Body noteRequest: NoteRequest):Response<NoteResponse>
+
+    @PUT("/note/{noteId}")
+    suspend fun updateNote(@Path("noteId") noteId:String,@Body noteRequest: NoteRequest):Response<NoteResponse>
+
+    @DELETE("/note/{nodeId}")
+    suspend fun deleteNote(@Path("noteId") noteId: String):Response<NoteResponse>
+}
---
+++ app/src/main/java/com/developer/android/dev/softcoderhub/androidapp/learnersnote/models/NoteRequest.kt
@@ -0,0 +1,6 @@
+package com.developer.android.dev.softcoderhub.androidapp.learnersnote.models
+
+data class NoteRequest(
+    val description: String,
+    val title: String
+)
