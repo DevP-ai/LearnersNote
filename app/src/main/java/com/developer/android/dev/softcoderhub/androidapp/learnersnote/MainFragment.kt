bd7fd0acfd7e5caa8398a4c54995a96f50b9aaf2
Devajit <patar1447@gmail.com>
2024-03-31 15:49:29 +0530

    Notes Design and Recyclerview Implemented

--- app/src/main/java/com/developer/android/dev/softcoderhub/androidapp/learnersnote/MainFragment.kt
+++ app/src/main/java/com/developer/android/dev/softcoderhub/androidapp/learnersnote/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.developer.android.dev.softcoderhub.androidapp.learnersnote.api.NotesAPI
+import com.developer.android.dev.softcoderhub.androidapp.learnersnote.databinding.FragmentMainBinding
 import com.developer.android.dev.softcoderhub.androidapp.learnersnote.utils.Constant.TAG
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
@@ -16,15 +17,20 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
-
+    private var _binding:FragmentMainBinding?= null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        _binding = FragmentMainBinding.inflate(layoutInflater)
 
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return binding.root
     }
 
-
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
